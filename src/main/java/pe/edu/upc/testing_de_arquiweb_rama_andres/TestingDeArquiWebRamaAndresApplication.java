7b25c457471b6c50a87da2a9056fce137cdac0fe
Andres H <andreshhizo1@gmail.com>
2024-09-18 15:46:17 -0500

    Commit para enviar todo en Andres 1

---
+++ src/main/java/pe/edu/upc/testing_de_arquiweb_rama_andres/TestingDeArquiWebRamaAndresApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.testing_de_arquiweb_rama_andres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDeArquiWebRamaAndresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingDeArquiWebRamaAndresApplication.class, args);
+    }
+
+}
