fcecb99d89238794e607740df7371fdb932fb07e
Andres H <andreshhizo1@gmail.com>
2024-09-18 15:54:57 -0500

    AÑADIENDO TODO

---
+++ src/test/java/pe/edu/upc/testing_de_arquiweb_rama_andres/TestingDeArquiWebRamaAndresApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.testing_de_arquiweb_rama_andres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingDeArquiWebRamaAndresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
